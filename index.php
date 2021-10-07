1ce9bd66035b7a4326fb04639e05263913ecc1ec
ika9 <nuraziqah92@gmail.com>
2021-10-07 08:42:57 +0800

    Create index.php
    constant

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+$jawatan = 'Pengurus';
+define('Jawatan' , 'Pengarah');
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Constants</title>
+    
+</head>
+<body>
+    <p>
+        Saya,selaku<?php echo Jawatan ;?>
+        dengan ini melantik Encik Ahmad sebagai
+        Penolong <?php echo $jawatan;?>.
+    </p>
+</body>
+</html>
